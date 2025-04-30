473a5899ddc4f7d51f89de9e8340eccbed06d1e8
sophiemwang <123534487+sophiemwang@users.noreply.github.com>
2025-04-30 07:58:24 -0400

    Update nyt_reviews.py

--- src/nyt_reviews.py
+++ src/nyt_reviews.py
@@ -1,1 +1,6 @@
-
+import pandas as pd
+NYTIMES_REVIEWS = pd.read_csv(
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vR6OOi2L9Ein88tZG6nNNeb6zxYfdYiO2id4x6ZmxwAqXQgkvtwjDIjFlfsJw7s0gYl5FDP57fXmNkD/pub?output=csv',
+  index_col=0, 
+  parse_dates=['date of review']
+)
